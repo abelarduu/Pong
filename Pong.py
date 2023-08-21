835eab6ce546f8ca3b89e24f247278d721a4316e
Abel Lucas <abelucas009@gmail.com>
2023-08-20 22:39:22 -0300

    melhorias

--- Pong.py
+++ Pong.py
@@ -12,7 +12,7 @@ class Object(object):
         self.col= col
         
     def move(self, cond1, cond2, cond3=None, cond4=None):
-        if cond1 and self.y >0: self.y-=2                     
+        if cond1 and self.y >3: self.y-=2                     
         if cond2 and self.y + self.h <= pyxel.height: self.y+=2 
         if cond3: self.x-=1                                  
         if cond4: self.x+=1                                     
@@ -28,6 +28,7 @@ class Object(object):
 class Game:
     def __init__(self):
         pyxel.init(100,100,"Pong")
+        self.play= False
         #Objetos
         self.player1= Object(3, pyxel.height/2 - (15/2), 5, 15, 7)
         self.player2= Object(pyxel.width -8, pyxel.height/2 - (15/2), 5, 15, 7)
@@ -35,39 +36,55 @@ class Game:
         self.ball= Object(pyxel.width/2 -3, pyxel.height/2 -3, 3, 3, 7)
         self.ball.eixX1, self.ball.eixX2= False,True
         self.ball.eixY1, self.ball.eixY2= False,True
-        
         self.listObject= [self.player1, self.player2, self.ball]
+        
+        pyxel.load("resources/pong.pyxres")
         pyxel.run(self.update, self.draw)
         
     def update(self):
-        self.player1.move(cond1= pyxel.btn(pyxel.KEY_W), cond2= pyxel.btn(pyxel.KEY_S))
-        self.player2.move(cond1= pyxel.btn(pyxel.KEY_UP), cond2= pyxel.btn(pyxel.KEY_DOWN))
-        self.ball.move(cond1= self.ball.eixY1, cond2= self.ball.eixY2,cond3= self.ball.eixX1, cond4= self.ball.eixX2)
+        if self.play:
+            self.player1.move(cond1= pyxel.btn(pyxel.KEY_W), cond2= pyxel.btn(pyxel.KEY_S))
+            self.player2.move(cond1= pyxel.btn(pyxel.KEY_UP), cond2= pyxel.btn(pyxel.KEY_DOWN))
+            self.ball.move(cond1= self.ball.eixY1, cond2= self.ball.eixY2,cond3= self.ball.eixX1, cond4= self.ball.eixX2)
 
-        self.ball.check_collision(self.player1)
-        self.ball.check_collision(self.player2)
+            self.ball.check_collision(self.player1)
+            self.ball.check_collision(self.player2)
 
-        if self.ball.y >= pyxel.height - self.ball.h: 
-            self.ball.eixY1= True
-            self.ball.eixY2=False  
-        if self.ball.y <= 0:
-            self.ball.eixY1=False
-            self.ball.eixY2 =True
+            if self.ball.y >= pyxel.height - self.ball.h-3: 
+                self.ball.eixY1= True
+                self.ball.eixY2=False  
+            if self.ball.y <= 3:
+                self.ball.eixY1=False
+                self.ball.eixY2 =True
 
-        if self.ball.check_collision(self.player1):
-            self.ball.eixX1=False
-            self.ball.eixX2 =True   
-        if self.ball.check_collision(self.player2): 
-            self.ball.eixX1= True
-            self.ball.eixX2=False  
-           
-        if pyxel.btn(pyxel.KEY_R):
-            self.ball.x, self.ball.y= pyxel.width/2 -3, pyxel.height/2 -3
-    
+            if self.ball.check_collision(self.player1):
+                self.ball.eixX1=False
+                self.ball.eixX2 =True   
+            if self.ball.check_collision(self.player2): 
+                self.ball.eixX1= True
+                self.ball.eixX2=False  
+               
+            if pyxel.btn(pyxel.KEY_R):
+                self.ball.x, self.ball.y= pyxel.width/2 -3, pyxel.height/2 -3
+        else:
+            if pyxel.btnr(pyxel.MOUSE_BUTTON_LEFT):
+                self.play= True
     def draw(self):
         pyxel.cls(0)
+        pyxel.mouse(True)
+        
+
+        pyxel.rect(0,0,pyxel.width,3,7)
+        pyxel.rect(0,pyxel.height-3,pyxel.width,3,7)
+        
         for obj in self.listObject:
             obj.draw()
             
+        if not self.play:
+            pyxel.blt(pyxel.width/2 - 62/2,pyxel.height/2 - 22/2,0,0,0,62,20)
+            pyxel.text(pyxel.width/2 - len("player1")/2 *4, pyxel.height/2 + 20,"player1",7)#pyxel.frame_count % 16)15
+            pyxel.text(pyxel.width/2 - len("player2")/2 *4, pyxel.height/2 +30,"player2", 7)#pyxel.frame_count % 16)25
+
+
 if __name__ == "__main__":
     Game()
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Pong
- 
+<div align="center">
+<header>
+<img src="img/icon.png" type="image/png" alt="Ache O Anel Icon">
+</header>
+
+
+
+<div>
+  <h3>Sobre o projeto</h3>
+  <div>
+  <h4>🚧⚠️Em desenvolvimento...⚠️🚧</h4>
+</div>
+  <p>Divirta-se tentando adivinhar aonde está o anel!
+Entretenha-se variando entre os modos do jogo ou escolha o seu modo favorito e curta a jogabilidade.</p>
+
+<p>Criado através da linguagem python, com objetivo de começar um projeto do mais simples possível, o “Ache o Anel” é um game com jogabilidade simples que utiliza a interface gráfica e cálculos matemáticos da game engine Pyxel para brincar com valores aleatórios.</p>
+
+  <p>
+  <p>
+    <a href="https://www.python.org/">
+      <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white" alt="Python V.3.9" ></a>
+    <a href="https://github.com/kitao/pyxel">
+      <img src="https://img.shields.io/badge/Pyxel-v1.9.12-blue?style=for-the-badge&logo=python&logoColor=white" alt="Pyxel V.1.9.12" ></a>
+  </p>
+</div>
+
+<div>
+  <h3>Resultado</h3>
+  <figure>
+    <img src="img/interface.png" type="image/png" alt="Interface do Game">
+   <img src="img/Ache O Anel.gif" type="image/gif" alt="Game gif" width=270px>
+  </figure>
+</div>
+</div>
